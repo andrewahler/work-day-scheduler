6dda8d9f2c42315d3a538520375bf11484cc3d89
Andrew Ahler <andrewahler@gmail.com>
2020-10-30 12:58:06 -0500

    added save button

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    })
+})
