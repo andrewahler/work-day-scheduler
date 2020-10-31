35c49e7a9f479e39f0d3ac709083755d32395c35
Andrew Ahler <andrewahler@gmail.com>
2020-10-31 11:18:42 -0500

    completed hourupdater

--- assets/script.js
+++ assets/script.js
@@ -22,5 +22,18 @@ $(document).ready(function() {
           }
         });
       }
+      hourUpdater();
+      var interval = setInterval(hourUpdater, 15000);
     
+      $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+      $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+      $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+      $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+      $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+      $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+      $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+      $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+      $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    
+      $("#currentDay").text(moment().format("dddd, MMMM Do"));
 })
