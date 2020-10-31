5aba8c663fb56f98048c0f7eb272c51faed0ae32
Andrew Ahler <andrewahler@gmail.com>
2020-10-31 10:35:34 -0500

    added hourupdater function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-    // listen for save button clicks
     $(".saveBtn").on("click", function() {
-      // get nearby values
       var value = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
-  
-      // save in localStorage
       localStorage.setItem(time, value);
     })
+    function hourUpdater() {
+        var currentHour = moment().hours();
+        $(".time-block").each(function() {
+          var blockHour = parseInt($(this).attr("id").split("-")[1]);
+          if (blockHour < currentHour) {
+            $(this).addClass("past");
+          } 
+          else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+          } 
+          else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+          }
+        });
+      }
+    
 })
